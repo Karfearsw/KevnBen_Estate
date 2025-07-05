76cc6d1f5476c862234cf9de5677bc5b467d433d
Karfearsw <100644909+Karfearsw@users.noreply.github.com>
2025-07-05 12:38:46 -0700

    Update app.js
    This update is for the form function

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-// Simple form submission placeholder
+
 document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  alert('Message sent! (This is a placeholder)');
+  const form = e.target;
+
+  fetch(form.action, {
+    method: 'POST',
+    body: new FormData(form),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      alert('Thank you! Your message has been sent.');
+      form.reset();
+    } else {
+      alert('Oops! There was a problem. Please try again.');
+    }
+  }).catch(error => {
+    alert('There was a problem submitting the form.');
+  });
 });
